9d53fa573d19856fbcd5be757d4d5a92b76f444f
Kevinjeet <Keving916@yahoo.com>
2023-04-24 17:14:04 -0700

    React manufacturerlist created

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import ManufacturerList from './ManufacturerList';
 
-function App() {
+function App(props) {
+
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="manufacturerlist">
+            <Route path="" element={<ManufacturerList manufacturers={props.manufacturers} />} />
+          </Route>
         </Routes>
       </div>
+
     </BrowserRouter>
   );
 }
--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -2,9 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+async function loadManufacturers() {
+  const response = await fetch('http://localhost:8100/api/manufacturers/');
+  if (response.ok) {
+    const data = await response.json();
+    root.render(
+      <React.StrictMode>
+        <App manufacturers={data.manufacturers} />
+      </React.StrictMode>
+    );
+    console.log(data, "-----");
+  } else {
+    console.error(response);
+  }
+}
+loadManufacturers();
