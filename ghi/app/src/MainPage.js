80186bb91345182a9b77c8d676465e0d147ad8b2
shanemccrackene <your_email_address@example.com>
2023-06-20 13:35:13 -0700

    Update

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,12 +1,15 @@
+import React from 'react';
+import carImage from './car.png';
+
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">CarCar</h1>
+      <h1 className="display-5 fw-bold">Cars.</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
-          The premiere solution for automobile dealership
-          management!
+          We keep you running
         </p>
+        <img src={carImage} alt="Car" className="car-image" style={{ width: '300px', height: 'auto' }} />
       </div>
     </div>
   );
